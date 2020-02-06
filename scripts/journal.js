c88ee9ddf080c99a8ee9f0701bca43ab1feec6f5
sophialawesome <59578660+sophialawesome@users.noreply.github.com>
2020-02-06 14:28:25 -0600

    printed date to DOM

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,25 +1,53 @@
 const journalEntry = {
-    date: "January 17, 2020",
-    conceptsCovered: "functions",
-    content: "It was a bit of a learning curve for me, but the practice examples in class helped me understand the concepts better.",
-    mood: "sad", 
-    entries:[],
-    
-    addJournalEntry: function(entry){
+
+    addJournalEntry: function (entry) {
         console.log(`${entry}`);
         this.entries.push(entry);
     },
 
-    displayJournalEntry: function()
-    {
-        for (let i=0; i<this.entries.length; i++){
+    displayJournalEntry: function () {
+        for (let i = 0; i < this.entries.length; i++) {
             console.log(this.entries[i]);
         }
 
     }
-        
+
 };
 
-journalEntry.addJournalEntry("I learned about objects.");
-journalEntry.addJournalEntry("I learned about functions and how they are utilized.");
-journalEntry.addJournalEntry("I learned about DOM.");
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+          <div class="entryLog">
+              <h1> ${journalEntry.conceptsCovered} </h1>
+              <section> ${journalEntry.content} </section>
+              <aside><b> ${journalEntry.date} </b></aside>
+          </div>
+      `
+}
+
+const renderJournalEntries = (entries) => {
+
+    const loggedEntriesContainer = document.querySelector(".entryLog");
+
+    for (let i = 0; i < entries.length; i++) {
+        const userInput = entries[i]
+        console.log(userInput);
+        loggedEntriesContainer.innerHTML += makeJournalEntryComponent(
+            userInput
+        )
+    }
+
+
+}
+
+fetch("http://127.0.0.1:3000/entries") 
+    .then(entries => entries.json()) 
+    .then(parsedEntries => {
+       
+        console.table(parsedEntries);
+
+        renderJournalEntries(parsedEntries);
+    })
+
+
+
+
